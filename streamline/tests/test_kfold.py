23a19eb66ba3f770fde7c185f1a6d2614142f69a
Harsh Bandhey <raptor419heavy@gmail.com>
2023-01-17 11:15:49 -0800

    Unit test for kfold phase, gitignore correction
    Former-commit-id: b281a611cc8e82e06b1d073f91a933de40e29752

---
+++ streamline/tests/test_kfold.py
@@ -0,0 +1,38 @@
+import shutil
+import pytest
+import pandas as pd
+from streamline.utils.dataset import Dataset
+from streamline.dataprep.kfold_partitioning import KFoldPartitioner
+
+dataset_valid = Dataset("./DemoData/demodata.csv", "Class")
+
+
+@pytest.mark.parametrize(
+    ("dataset", "partition_method", "experiment_path", "exception"),
+    [
+        ("", "Random", "./tests/", Exception),
+        (dataset_valid, "something", "./tests/", Exception),
+        (dataset_valid, "Group", "./tests/", Exception),
+    ],
+)
+def test_invalid_kfold(dataset, partition_method, experiment_path, exception):
+    with pytest.raises(exception):
+        KFoldPartitioner(dataset, partition_method, experiment_path)
+
+
+def test_valid_kfold():
+    partition_method, experiment_path = "Random", "./tests/"
+    kfold = KFoldPartitioner(dataset_valid, partition_method, experiment_path)
+    train_dfs, test_dfs = kfold.cv_partitioner(n_splits=5, random_state=42, return_dfs=False, save_dfs=False,
+                                               partition_method=partition_method)
+    assert (train_dfs is None)
+    assert (test_dfs is None)
+    train_dfs, test_dfs = kfold.cv_partitioner(n_splits=5, random_state=42, return_dfs=True, save_dfs=False,
+                                               partition_method=partition_method)
+    for df in train_dfs + test_dfs:
+        print(len(df))
+        assert (type(df) is pd.DataFrame and len(df) != 0)
+
+    train_dfs, test_dfs = kfold.cv_partitioner(n_splits=5, random_state=42, return_dfs=False, save_dfs=True,
+                                               partition_method=partition_method)
+    # shutil.rmtree('./tests/')
