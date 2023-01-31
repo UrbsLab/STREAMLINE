df07db6db5db8ce644d78e11cf6428e2e6c771be
Harsh Bandhey <raptor419heavy@gmail.com>
2023-01-31 13:18:01 -0800

    added genetic programming
    Former-commit-id: 905a3b457833fbe423e239bd79b178cf1cb7144f

---
+++ streamline/models/genetic_programming.py
@@ -0,0 +1,33 @@
+from abc import ABC
+from streamline.modeling.basemodel import BaseModel
+from streamline.modeling.parameters import get_parameters
+from gplearn.genetic import SymbolicClassifier as GP
+
+
+class GPClassifier(BaseModel, ABC):
+    def __init__(self, cv_folds=5, scoring_metric='balanced_accuracy',
+                 metric_direction='maximize', random_state=None, cv=None, n_jobs=None):
+        super().__init__(GP, "Artificial Neural Network", cv_folds, scoring_metric, metric_direction, random_state, cv)
+        self.param_grid = get_parameters(self.model_name)
+        self.param_grid['random_state'] = [random_state, ]
+        self.small_name = "GP"
+        self.color = "purple"
+        self.n_jobs = n_jobs
+
+    def objective(self, trial, params=None):
+        self.params = {'population_size': trial.suggest_int('population_size', self.param_grid['population_size'][0],
+                                                            self.param_grid['population_size'][1]),
+                       'generations': trial.suggest_int('generations', self.param_grid['generations'][0],
+                                                        self.param_grid['generations'][1]),
+                       'tournament_size': trial.suggest_int('tournament_size', self.param_grid['tournament_size'][0],
+                                                            self.param_grid['tournament_size'][1]),
+                       'function_set': trial.suggest_categorical('function_set', self.param_grid['function_set']),
+                       'init_method': trial.suggest_categorical('init_method', self.param_grid['init_method']),
+                       'parsimony_coefficient': trial.suggest_float('parsimony_coefficient',
+                                                                    self.param_grid['parsimony_coefficient'][0],
+                                                                    self.param_grid['parsimony_coefficient'][1]),
+                       'feature_names': trial.suggest_categorical('feature_names', self.param_grid['feature_names']),
+                       'low_memory': trial.suggest_categorical('low_memory', self.param_grid['low_memory']),
+                       'random_state': trial.suggest_categorical('random_state', self.param_grid['random_state'])}
+        mean_cv_score = self.hypereval(trial)
+        return mean_cv_score
