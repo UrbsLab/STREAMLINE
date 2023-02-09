36045127f1a00695586b6f86c6a9278ad7449f29
Harsh Bandhey <raptor419heavy@gmail.com>
2023-02-09 13:49:30 -0800

    Runner for OSA

---
+++ run_osa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p defq
+#SBATCH --job-name=OSA_JOB
+#SBATCH --mem=20G
+#SBATCH -o OSA.o
+#SBATCH -e OSA.e
+#SBATCH --time=1-20:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=100
+srun python run_osa.py
