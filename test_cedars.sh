54c3065d8ebb7ac63a20f242455771068fff6708
Harsh Bandhey <raptor419heavy@gmail.com>
2023-05-29 11:17:47 -0700

    added test script

---
+++ test_cedars.sh
@@ -0,0 +1,12 @@
+python run.py --data-path ./data/DemoData --out-path demo --exp-name demo_full --do-till-report --class-label Class --inst-label InstanceID --algorithms=NB,LR,DT --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --data-path ./data/DemoData --out-path demo --exp-name demo --do-eda --class-label Class --inst-label InstanceID --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --out-path demo --exp-name demo --do-dataprep --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --out-path demo --exp-name demo --do-feat-imp --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --out-path demo --exp-name demo --do-feat-sel --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --out-path demo --exp-name demo --do-model --algorithms NB,LR,DT --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --out-path demo --exp-name demo --do-stats --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --out-path demo --exp-name demo --do-compare-dataset --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --out-path demo --exp-name demo --do-report --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --rep-path ./data/DemoRepData --dataset ./data/DemoData/hcc-data_example_custom_rep.csv --out-path demo --exp-name demo --do-replicate --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --rep-path ./data/DemoRepData --dataset ./data/DemoData/hcc-data_example_custom_rep.csv --out-path demo --exp-name demo --do-rep-report --run-cluster SLURMOld --res-mem 4 --queue defq
+python run.py --out-path demo --exp-name demo --do-clean --del-time --del-old-cv --run-cluster SLURMOld --r
