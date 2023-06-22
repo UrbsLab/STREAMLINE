916f08c3acd7fabd742f266a9a085b9df672f69c
Harsh Bandhey <raptor419heavy@gmail.com>
2023-06-22 13:45:05 -0700

    Added UGE and HTCondor specific settings

--- streamline/utils/cluster.py
+++ streamline/utils/cluster.py
@@ -1,11 +1,15 @@
 from dask.distributed import Client, LocalCluster
-from dask_jobqueue import SLURMCluster, LSFCluster, SGECluster, PBSCluster
+from dask_jobqueue import SLURMCluster, LSFCluster, SGECluster
+from dask_jobqueue import HTCondorCluster, MoabCluster, OARCluster, PBSCluster
 
 cluster_dict = {
-    'SLURM': SLURMCluster,
+    'HTCondor': HTCondorCluster,
     'LSF': LSFCluster,
-    'SGE': SGECluster,
+    'Moab': MoabCluster,
+    'OAR': OARCluster,
     'PBS': PBSCluster,
+    'SGE': SGECluster,
+    'SLURM': SLURMCluster,
     'Local': LocalCluster
 }
 
@@ -27,6 +31,20 @@ def get_cluster(cluster_type='SLURM', output_path=".", queue='defq', memory=4):
                                  walltime="12:00",
                                  log_directory=output_path + "/dask_logs/")
             cluster.adapt(maximum_jobs=400)
+        elif cluster_type == 'UGE':
+            cluster = SGECluster(queue=queue,
+                                   cores=1,
+                                   memory=str(memory) + "G",
+                                   resource_spec="mem_free=" + str(memory) + "G",
+                                   walltime="12:00:00",
+                                   log_directory=output_path + "/dask_logs/")
+            cluster.adapt(maximum_jobs=400)
+        elif cluster_type == 'HTCondor':
+            cluster = HTCondorCluster(cores=1,
+                                      disk=str(memory) + "G",
+                                      memory=str(memory) + "G",
+                                      log_directory=output_path + "/dask_logs/")
+            cluster.adapt(maximum_jobs=400)
         else:
             try:
                 cluster = cluster_dict[cluster_type](queue=queue,
@@ -38,7 +56,8 @@ def get_cluster(cluster_type='SLURM', output_path=".", queue='defq', memory=4):
             except KeyError:
                 raise Exception("Unknown or Unsupported Cluster Type")
         client = Client(cluster)
-    except Exception:
+    except Exception as e:
+        print(e)
         raise Exception("Exception: Unknown Exception")
     # print("Running dask-cluster")
     # print(client.scheduler_info())
